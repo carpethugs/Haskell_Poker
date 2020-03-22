0f5c65d1a3db39f2c91b407b91f303e64a93aa7f
unknown <calvin@stuntfactory.com>
2020-03-22 15:30:14 -0400

    Added 'notation' shifting

--- Poker.hs
+++ Poker.hs
@@ -7,3 +7,18 @@ module Poker where
 
 
     deal cards =  
+
+    -- List shifting (simple notation is the notation provide by the prof, standard notation is the one we will be working on in this program)
+    shiftToSimpleNotationFunc x = do
+        if suit x ==  1 then x+11
+        else x-2
+    shiftToSimpleNotation list = map shiftToSimpleNotationFunc list
+    shiftToStandardNotationFunc x = do
+        if suit x == 12 then x-11
+        else x+2
+    shiftToStandardNotation list = map shiftToStandardNotationFunc list
+    
+    --card operations (assumes standard notation)
+    suit x = mod x 13
+    value x = div x 13
+    
