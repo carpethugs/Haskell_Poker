61aabfdce1b02f21e783a98feeb19c44db8f9cd3
carpethugs <danmezh@gmail.com>
2020-03-22 18:21:17 -0400

    Changed file name

--- Poker.hs
+++ Poker.hs
@@ -1,9 +1,11 @@
 module Poker where
-    
+
     data Hand = Hand Int Int Int Int Int
     data Card = Card Char Int
 
-    getCard(Card Suit Val) = 
+    getCard(Card Suit Val) = Suit ++ show Val
 
+    getVal card= (card `mod` 13)
 
     deal cards =  
+
