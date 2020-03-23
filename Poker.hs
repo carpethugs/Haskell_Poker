e04021f38084a6621b4e9cfbcce2247a4c1f39cf
unknown <calvin@stuntfactory.com>
2020-03-22 21:23:31 -0400

    Added helper functions

--- Poker.hs
+++ Poker.hs
@@ -1,11 +1,7 @@
 module Poker where
     
-    -- data Hand = Hand Int Int Int Int Int
-    -- data Card = Card Char Int
 
-    -- getCard(Card Suit Val) = 
-
-    deal cards =  
+    --work on this later ->>> deal cards =  
        
 
     -- List shifting (simple notation is the notation provide by the prof, standard notation is the one we will be working on in this program)
@@ -19,10 +15,74 @@ module Poker where
     shiftToStandardNotation list = map shiftToStandardNotationFunc list
     
     --card operations (assumes standard notation)
-    suit x = mod x 13
-    value x = div x 13
+    value x = mod x 13
+    suit x = div x 13
 
+    {-
+    Royal flush = 0
+    straight flush = 1
+    four of a kind = 2
+    full house = 3
+    flush = 4
+    straigh = 5
+    three of a kind =6
+    two pair = 7
+    one pair = 8
+    high card = 9
+    -}
+    determineHandType hand = do
+        if ( isRoyalFlush hand ) then 0
+        else 9
     
+    isRoyalFlush hand = do
+        if ( isStraightFlush hand && containsValueWithFunc hand 12 value) then True
+        else False
+
+    isStraightFlush hand = do
+        if( isStraight hand && isFlush hand ) then True
+        else False
+    
+    isStraight hand = do
+        let handByValue = sortHandByMethod hand [] sortByValue
+        isDescendingFrom ((tail) handByValue) ((head) handByValue)
+    
+    isDescendingFrom hand desFrom
+        | null hand = True
+        | not (value ((head) hand) == ((value) desFrom) - 1) = False  
+        | otherwise = isDescendingFrom ((tail) hand) ((head) hand)
+    
+
+    ---------------Helper functions-------------------
+
+    --hand is the list that you are serching
+    --value is the value you are looking for
+    --func is the func by which the card is evaluated to(pass in suit or value)
+    containsValueWithFunc hand value func 
+        | hand == [] = False
+        | func ((head) hand) == value = True
+        | otherwise = containsValueWithFunc ((tail) hand) value func
+    
+    --returns true is card1 is larger or equal to card 2(same for sort by suit)
+    sortByValue card1 card2 = do
+        if value card1 > value card2 then True
+        else if value card1 < value card2 then False
+        else if suit card1 < suit card2 then False
+        else True
+
+    sortBySuit card1 card2 = do
+        if suit card1 > suit card2 then True
+        else if suit card1 < suit card2 then False
+        else if value card1 < value card2 then False
+        else True
+    
+    sortHandByMethod unsorted sorted func
+        | length unsorted == 1 = sorted ++ unsorted
+        | ((func) ((head) unsorted) (unsorted !! 1)) == False = do
+            let eleToSwapUp = head unsorted
+            let eleToSwapDown = unsorted !! 1
+            let partiallySorted = (sorted) ++ [eleToSwapDown,eleToSwapUp] ++ (tail ((tail) unsorted))
+            sortHandByMethod partiallySorted [] func
+        | otherwise = sortHandByMethod (tail unsorted) (sorted ++ [((head) unsorted)]) func
 
 -----------------------------------------------------------------------------------------------------------------------------------------------------------
 --Daniels Side
